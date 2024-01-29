53fae881d5d25898e747c78161afc186e25be61c
Ninad527 <ninadmungekar180@gmail.com>
2024-01-29 13:03:05 +0530

    Added Routes

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ const http = require("http");
 
 const port = 8081;
 
+const toDolist= ["Complete node byte", "Play Cricket"];
+
 http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1> Hello!!!, This is from my server </h1>");
+    const {method, url}= request;
+    if(url === "/todos"){
+        if(method === "GET"){
+            response.writeHead(200);
+            response.write(toDolist.toString())
+        }
+        else if(method === "POST"){
+            let body = " ";
+            request.on("error", ()=> {
+                console.log(err)
+            }).on("data", (chunk)=> {
+                body += chunk;
+                console.log(chunk);
+            }).on("end", ()=> {
+                body = JSON.parse(body);
+                console.log("data: ", body);
+            });
+        }
+        else{
+            response.writeHead(404);
+        }
+    }
+    else{
+        response.writeHead(404);
+    }
+    
     response.end();
-}).listen(port, ()=> {
+    })
+    
+   .listen(port, ()=> {
     console.log(`Nodejs Server Started on port ${port}`);
 });
-
-//http://localhost:8081
